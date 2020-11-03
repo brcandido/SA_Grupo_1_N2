a9a5e30b4649dc7c57dbb1f39701235083d87e88
brcandido <bruno_r_candido@estudante.sc.senai.br>
2020-11-03 19:37:55 -0300

    adicionando todas as funções

--- scripts.js
+++ scripts.js
@@ -114,14 +114,14 @@ function validateLogin() {
   passwordLogin.value = "";
 }
 
-
+//###################################################################################################
 
 //CRUD para serviço
 let checkin = document.getElementById("dataCheckIn")
 let checkout = document.getElementById("dataCheckOut")
 let quartosDuplos = document.getElementById("quartosDuplos")
 let quartosTriplos = document.getElementById("quartosTriplos")
-let cafe = document.getElementsByName("cafe")
+let regime = document.getElementsByName("regime")
 let vaga = document.getElementsByName("vaga")
 let obs = document.getElementById("obs")
 let cliente = document.getElementById("cliente")
@@ -178,19 +178,22 @@ function cadastraReserva() {
       }
     ]
   }
-  function Reserva(cliente, checkin, checkout, quartosDuplos, quartosTriplos, cafe, garagem, obs) {
+  function Reserva(cliente, checkin, checkout, quartosDuplos, quartosTriplos, regime, garagem, obs) {
     this.id = geraID()
     this.cliente = cliente
     this.checkin = checkin
     this.checkout = checkout
     this.quartosDuplos = quartosDuplos
     this.quartosTriplos = quartosTriplos
-    this.cafe = cafe
+    this.regime = regime
     this.garagem = garagem
     this.obs = obs
   }
 
   let quartos = JSON.parse(localStorage.getItem('arrayQuartos')) ? JSON.parse(localStorage.getItem('arrayQuartos')) : estruturaQuartos
+  let dataAtual = new Date()
+  let partes = dataCheckIn.value.split('-')
+  let dataCheck = new Date(partes[0], partes[1] - 1, partes[2])
 
   let cliente = ''
   JSON.parse(localStorage.getItem('arrayCadastros')).forEach(element => {
@@ -199,10 +202,10 @@ function cadastraReserva() {
     }
   })
 
-  let opcaoCafe = ''
-  cafe.forEach(element => {
+  let opcaoRegime = ''
+  regime.forEach(element => {
     if (element.checked) {
-      opcaoCafe = element.value
+      opcaoRegime = element.value
     }
   })
 
@@ -217,11 +220,13 @@ function cadastraReserva() {
   let triplos = Number(quartosTriplos.value)
 
   if (dataCheckOut.value > dataCheckIn.value && (duplos > 0 || triplos > 0)) {
-    let reserva = new Reserva(cliente, dataCheckIn.value, dataCheckOut.value, duplos, triplos, opcaoCafe, opcaoVaga, obs.value)
-    // console.log(reserva)
-    alocaQuartos(quartos.duplos, 'quartosDuplos', reserva)
-    alocaQuartos(quartos.triplos, 'quartosTriplos', reserva)
-    localStorage.setItem('arrayQuartos', JSON.stringify(quartos))
+    if(dataCheckIn >= dataAtual){
+      let reserva = new Reserva(cliente, dataCheckIn.value, dataCheckOut.value, duplos, triplos, opcaoRegime, opcaoVaga, obs.value)
+      // console.log(reserva)
+      alocaQuartos(quartos.duplos, 'quartosDuplos', reserva)
+      alocaQuartos(quartos.triplos, 'quartosTriplos', reserva)
+      localStorage.setItem('arrayQuartos', JSON.stringify(quartos))
+    }
   }
 
   function alocaQuartos(arrayQuartos, numQuartos, reserva) {
@@ -239,12 +244,14 @@ function cadastraReserva() {
                 if (comparaDatas(reserva.checkin, arrayQuartos[j].reservas[k].checkout) || comparaDatas(arrayQuartos[j].reservas[k].checkin, reserva.checkout)) {
                   arrayQuartos[j].reservas.push(reserva)
                   count++
+                  alert(`O id de sua reserva é ${reserva.id}`)
                 }
               }
             }
           } else if (count < reserva[numQuartos]) {
             arrayQuartos[j].reservas.push(reserva)
             count++
+            alert(`O id de sua reserva é ${reserva.id}`)
           }
         }
         if (count == 0) {
@@ -266,9 +273,10 @@ function cadastraReserva() {
 let dataInicial = document.getElementById('dataInicial')
 let dataFinal = document.getElementById('dataFinal')
 let tipoQuarto = document.getElementById('tipoQuarto')
+let id = document.getElementById('ID')
 
 
-function buscaReserva(){
+function buscaReservaData(){
   let arrayQuartos = JSON.parse(localStorage.getItem('arrayQuartos'))
   let reservas = []
   if(tipoQuarto.value === 'duplo'){
@@ -295,4 +303,175 @@ function buscaReserva(){
   
   console.log(reservas)
   return reservas
+}
+
+
+
+function buscaReservaId(){
+  let arrayQuartos = JSON.parse(localStorage.getItem('arrayQuartos'))
+  let reservas =[]
+  function percorreQuartos(arrayQuartos){
+    for (let i = 0; i <arrayQuartos.length ; i++){
+      if(arrayQuartos[i].reservas.length > 0){
+        for (let j = 0; j < arrayQuartos[i].reservas.length; j++){
+          if (arrayQuartos[i].reservas[j].id === id.value){
+             arrayQuartos[i].reservas[j].num = arrayQuartos[i].num
+            reservas.push(arrayQuartos[i].reservas[j])  
+          }
+        }
+      }
+    }
+  }
+  percorreQuartos(arrayQuartos.duplos)
+  percorreQuartos(arrayQuartos.triplos)
+  console.log(reservas)
+  return reservas
+}
+
+
+function atualizaReserva() {
+  let arrayQuartos = JSON.parse(localStorage.getItem('arrayQuartos'))
+  let reservas = []
+  let opcaoRegime = ''
+  regime.forEach(element => {
+    if (element.checked) {
+      opcaoRegime = element.value
+    }
+  })
+ 
+  let opcaoVaga = ''
+  vaga.forEach(element => {
+    if (element.checked) {
+      opcaoVaga = element.value
+    }
+  })
+
+  function percorreQuartos(arrayQuartos){
+    for (let i = 0; i <arrayQuartos.length ; i++){
+      if(arrayQuartos[i].reservas.length > 0){
+        for (let j = 0; j < arrayQuartos[i].reservas.length; j++){
+          if (arrayQuartos[i].reservas[j].id === id.value){
+             arrayQuartos[i].reservas[j].regime = opcaoRegime
+             arrayQuartos[i].reservas[j].garagem = opcaoVaga
+            reservas.push(arrayQuartos[i].reservas[j])  
+          }
+        }
+      }
+    }
+  }
+
+  percorreQuartos(arrayQuartos.duplos)
+  percorreQuartos(arrayQuartos.triplos)
+  buscaReservaId()
+  console.log(reservas)
+  localStorage.setItem('arrayQuartos', JSON.stringify(arrayQuartos))
+}
+
+function apagaReserva() {
+  let arrayQuartos = JSON.parse(localStorage.getItem('arrayQuartos'))
+  function percorreQuartos(arrayQuartos){
+    for (let i = 0; i <arrayQuartos.length ; i++){
+      if(arrayQuartos[i].reservas.length > 0){
+        for (let j = 0; j < arrayQuartos[i].reservas.length; j++){
+          if (arrayQuartos[i].reservas[j].id === id.value){
+            delete arrayQuartos[i].reservas[j]
+            let organizaArray = arrayQuartos[i].reservas.filter(element => element)
+            arrayQuartos[i].reservas = organizaArray 
+          }
+        }
+      }
+    }
+  }
+  percorreQuartos(arrayQuartos.duplos)
+  percorreQuartos(arrayQuartos.triplos)
+  localStorage.setItem('arrayQuartos', JSON.stringify(arrayQuartos))
+}
+
+//################################################################################################################
+
+
+//CRUD funcionario
+let RG = document.getElementById('RG')
+let cargo = document.getElementById('cargo')
+const funcionarios = [{login: 'admin', password: 'admin', status: false}]
+
+function limpaCampos2() {
+  username.value = null
+  senha.value = null
+  confirmaSenha.value = null
+  RG.value = null
+  cargo.value = null
+  nome.value = null
+}
+
+function cadastraFuncionario(password = senha.value, confirmaPassword = confirmaSenha.value) {
+  function Funcionario(login, password, RG, name, cargo, status = false) {
+    this.login = login
+    this.password = password
+    this.RG = RG
+    this.name = name
+    this.cargo = cargo
+    this.status = status
+  }
+
+  if (username.value && senha.value) {
+    if (password === confirmaPassword) {
+      let funcionario = new Funcionario(username.value, senha.value, RG.value, nome.value, cargo.value)
+
+      if (localStorage.getItem('arrayFuncionarios')) {
+        let arrayFuncionarios = JSON.parse(localStorage.getItem('arrayFuncionarios'))
+        let check = arrayFuncionarios.map(item => item.login)
+        if (check.includes(funcionario.login)) {
+          return alert('O nome de usuario está indisponivel')
+        } else {
+          arrayFuncionarios.push(funcionario)
+          localStorage.setItem('arrayFuncionarios', JSON.stringify(arrayFuncionarios))
+          limpaCampos2()
+        }
+      } else {
+        funcionarios.push(funcionario)
+        localStorage.setItem('arrayFuncionarios', JSON.stringify(funcionarios))
+        limpaCampos2()
+      }
+    } else {
+      confirmaSenha.value = null
+      return alert('As senhas não coincidem')
+    }
+  } else {
+    return alert('Favor preencher todos os campos do cadastro')
+  }
+}
+
+function buscaFuncionario() {
+  let arrayFuncionarios = JSON.parse(localStorage.getItem('arrayFuncionarios'))
+  let flag = true
+  let funcionario = arrayFuncionarios.filter(element => {
+    if (element.login === username.value) {
+      flag = false
+      return element
+    }
+  });
+  if (flag) {
+    return alert('Este usuário não foi cadastrado')
+  }
+  return funcionario
+}
+
+function atualizarCadastroFuncionario() {
+  let arrayFuncionarios = JSON.parse(localStorage.getItem('arrayFuncionarios'))
+  let funcionario = arrayFuncionarios.findIndex(element => element.login === buscaFuncionario()[0].login)
+  if (senha.value) arrayFuncionarios[funcionario].password = senha.value;
+  if (cargo.value) arrayFuncionarios[funcionario].cargo = cargo.value;
+  if (nome.value) arrayFuncionarios[funcionario].name = nome.value;
+  localStorage.setItem('arrayFuncionarios', JSON.stringify(arrayFuncionarios))
+  limpaCampos()
+}
+
+function apagaFuncionario() {
+  let arrayFuncionarios = JSON.parse(localStorage.getItem('arrayFuncionarios'))
+  let funcionario = arrayFuncionarios.findIndex(element => element.login === buscaFuncionario()[0].login)
+  delete arrayFuncionarios[funcionario]
+  let organizaArray = arrayFuncionarios.filter(element => element)
+  localStorage.setItem('arrayFuncionarios', JSON.stringify(organizaArray))
+  limpaCampos()
 }
